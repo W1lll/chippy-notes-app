5480d4577adcba4c518081918d2f1d75d8de6b3f
Will Foster <w1ll.fost3r@gmail.com>
2023-12-08 05:20:58 +0000

    Added more markup for templating + basic styling/layout. Also added partial for navbar – componentization will help.

--- views/noteView.php
+++ views/noteView.php
@@ -3,23 +3,58 @@
 <html>
 <head>
     <title>Note Display</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Simple note jotting app.">
+    <meta name="author" content="Group 14">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
 </head>
 <body>
-    <form action="index.php" method="POST">
-        <input type="text" name="inputText" id="inputText">
-        <input type="submit" style="display: none;">
-    </form>
+    <?php include "partials/sideBar.php"; ?>
+    <div class="content-wrapper">
+        
+        <?php if (isset($displayText)): ?>
+            <div class="note-wrapper">
+                <header>
+                    <input type="text" name="noteInputTitle" id="noteInputTitle">
+                </header>
+                <p>Entered Text: <?php echo htmlspecialchars($displayText); ?></p>
+            </div>
+        <?php endif; ?>
 
-    <?php if (isset($displayText)): ?>
-        <p>Entered Text: <?php echo htmlspecialchars($displayText); ?></p>
-    <?php endif; ?>
+        <form action="index.php" method="POST" class="text-bar">
+            <input type="text" name="noteInputText" id="noteInputText">
+            <button type="submit">
+                <span class="material-symbols-outlined">arrow_upward_alt</span>
+            </button>
+        </form>
+        
+        <script>
+            document.getElementById("noteInputText").addEventListener("keypress", function (e) {
+                if (e.key === "Enter") {
+                    this.form.submit();
+                }
+            });
 
-    <script>
-        document.getElementById('inputText').addEventListener('keypress', function (e) {
-            if (e.key === 'Enter') {
-                this.form.submit();
+            function handleKeyPress(e) {
+                var noteInputText = document.getElementById("noteInputText");
+                var activeElement = document.activeElement;
+
+                if (!activeElement || activeElement.tagName !== 'INPUT' || activeElement.type !== 'text') {
+                    if (document.activeElement !== noteInputText) {
+                        noteInputText.focus();
+                        e.preventDefault();
+
+                        if (e.key.length === 1) {
+                            noteInputText.value += e.key;
+                        }
+                    }
+                }
             }
-        });
-    </script>
+
+            document.addEventListener("keypress", handleKeyPress);
+        </script>
+    </div>
 </body>
 </html>
---
+++ views/partials/sideBar.php
@@ -0,0 +1,17 @@
+<!-- partials/sideBar.php -->
+<div class="nav">
+    <header>
+        Notes
+    </header>
+    <nav>
+        <button>
+            Note A
+        </button>
+        <button>
+            Note B
+        </button>
+        <button>
+            Super long note title to check text wrapping.
+        </button>
+    </nav>
+</div>
