2992ce86fd76f68c8fe38785cba3e00242ec00a8
Hussenaa02 <153169161+Hussenaa02@users.noreply.github.com>
2023-12-26 16:57:51 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,9 @@
+<header>
+        <nav class="navbar">
+            <ul class="nav-links">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="all-notes.php">Notes</a></li>
+                <li><a href="login.php">Register/Login</a></li>
+            </ul>
+        </nav>
+    </header>
